0cb4f59db95d0854ce72d1b27f3eabbcec8f06ba
Henes <dlvudrkd60@gmail.com>
2025-07-25 16:35:55 +0900

    Fix: Refactor LiteYouTubeComponent.tsx based on provided example

--- src/components/LiteYouTubeComponent.tsx
+++ src/components/LiteYouTubeComponent.tsx
@@ -1,33 +1,23 @@
+'use client';
+
 import React, { useEffect } from 'react';
 
-interface LiteYouTubeComponentProps {
+interface Props {
   videoId: string;
   title: string;
   start?: number;
 }
 
-const LiteYouTubeComponent: React.FC<LiteYouTubeComponentProps> = ({ videoId, title, start }) => {
+const LiteYouTubeComponent: React.FC<Props> = ({ videoId, title, start }) => {
   useEffect(() => {
-    // Dynamically import lite-youtube-embed only on the client side
-    if (typeof window !== 'undefined' && !customElements.get('lite-youtube')) {
-      import('lite-youtube-embed');
-    }
+    import('lite-youtube-embed'); // 웹 컴포넌트 동적 로딩
   }, []);
 
-  return React.createElement(
-    'lite-youtube',
-    {
-      videoid: videoId,
-      playlabel: title,
-      params: start ? `start=${start}` : '',
-      style: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
-      },
-    }
-  );
+  return React.createElement('lite-youtube', {
+    videoid: videoId,
+    playlabel: title,
+    params: start ? `start=${start}` : '',
+  });
+};
 
 export default LiteYouTubeComponent;
